011b35ebc429939f26a60fdaa57a99e7dfb4e8e2
Shaikh Zahid Raza <work.zahidraza@gmail.com>
2026-06-30 20:48:54 +0530

    Refactor: Comment out GSAP animation code for future reference

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 let box1 = document.querySelector(".box1");
 let box2 = document.querySelector(".box2");
 
-gsap.to(box1, {
-  x: 700,
-  delay: 1,
-  duration: 2,
-  rotate: 360,
-});
+//basic gsap
+// gsap.to(box1, {
+//   x: 700,
+//   delay: 1,
+//   duration: 2,
+//   rotate: 360,
+// });
 
-gsap.from(box2, {
-  x: 700,
-  duration: 2,
-  delay: 1,
-  rotate:360
-});
+// gsap.from(box2, {
+//   x: 700,
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+// });
+
+// gsap timeline help to execute one in order wise
+const tl = gsap.timeline();
+// tl.to(box1, {
+//   x: 700,
+//   delay: 0.5,
+//   duration: 1,
+//   rotate: 360,
+//   stagger:1
+// });
+//gets current yoyo state
+
+// tl.from(box2, {
+//   y: -100,
+//   duration: 1,
+//   delay: 0.5,
+// //   rotate: 360,
+//   yoyo:true
+  
+// });
+
+
+//
