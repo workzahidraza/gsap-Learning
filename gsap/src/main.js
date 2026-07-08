e82b454b9e1d5c4dbaa6cdaa0d9adab4a6794955
Shaikh Zahid Raza <work.zahidraza@gmail.com>
2026-07-09 01:33:45 +0530

    Refactor: Update index.html and index.css to enhance box layout and styling; modify main.js for improved animation sequencing

--- gsap/src/main.js
+++ gsap/src/main.js
@@ -1,13 +1,74 @@
 import gsap from "gsap";
 import CustomEase from "gsap/CustomEase";
-gsap.set(".box1 h1", {
-    x:"-100vw"
-});
+// gsap.set(".box1 h1", {
+//     x:"-100vw"
+// });
 
-gsap.from(".box1 h1", {
-  x: "101vw",
-  duration: 3,
-  // delay:0.5,
-  ease: "none",
-  repeat: -1,
-});
+// gsap.from(".box1 h1", {
+//   x: "101vw",
+//   duration: 3,
+//   // delay:0.5,
+//   ease: "none",
+//   repeat: -1,
+// });
+
+//stagger
+// gsap.to(".boxes .box", {
+//   x: 500,
+//   duration: 0.9,
+//   delay:0.2,
+//   stagger: 0.2,
+// });
+
+// gsap.from(".boxes .box",{
+//   x:500,
+//   duration:0.9,
+//   stagger:0.3,
+// })
+
+// gsap.to(".boxes .box", {
+//   x: 500,
+//   duration: 0.9,
+//   delay: 0.2,
+//   stagger: {
+//     from: "center",
+//     amount: 0.3,
+//   },
+// });
+
+const tl = gsap.timeline();
+
+// < , -=0.2  +=0.2, labels = "any random words"
+
+tl.to(
+  ".box1",
+  {
+    x: 500,
+    duration: 0.9,
+  },
+  "zahid",
+)
+  .to(
+    ".box2",
+    {
+      x: 500,
+      duration: 0.9,
+    },
+    "zah",
+  )
+  .to(
+    ".box3",
+    {
+      x: 500,
+      duration: 0.9,
+    },
+    "zahid",
+  )
+  .to(
+    ".box4",
+    {
+      x: 500,
+      duration: 0.9,
+    },
+    "zah",
+  );
